426ab8a5201c8cbf1f346e1530a311516daf92de
Raquel Oliveira <hackel@lcc.ufrn.br>
2019-11-18 11:48:02 -0300

    configure retrofit
    Configure use of retrofit do cosume data from https://jsonplaceholder.typicode.com/photos

--- app/src/main/java/com/example/trabalho2/MainActivity.kt
+++ app/src/main/java/com/example/trabalho2/MainActivity.kt
@@ -2,11 +2,40 @@ package com.example.trabalho2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    var retrofit: Retrofit? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        retrofit = Retrofit.Builder()
+            .baseUrl("https://jsonplaceholder.typicode.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    }
+
+    private fun getPhotos(){
+        var photoService: PhotoService = retrofit!!.create(PhotoService::class.java)
+        var call: Call<List<Photo>> = photoService.getPhotos()
+
+        call.enqueue(object : Callback<List<Photo>> {
+            override fun onFailure(call: Call<List<Photo>>, t: Throwable) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onResponse(call: Call<List<Photo>>, response: Response<List<Photo>>) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/example/trabalho2/Photo.kt
@@ -0,0 +1,9 @@
+package com.example.trabalho2
+
+class Photo (
+    var albumId: Int,
+    var id: Int,
+    var title: String,
+    var url: String,
+    var thumbnailUrl: String){
+}
